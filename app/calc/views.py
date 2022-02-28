45868063ad17faa2e6279f9ed404e378d2275753
smudhiga <smudhiganti@phoenixcontact.com>
2022-02-27 21:27:45 -0500

    Extending templates

--- app/calc/views.py
+++ app/calc/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-  return render(request, 'home.html', { 'name': 'Sai Krishna Reddy'})
+  return render(request, 'home.html', { 'name': 'Sai Krishna Reddy', 'title': 'Sai'})
