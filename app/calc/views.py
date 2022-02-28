08613bfca6b00fd40ae074b14b15f2ae1c402f32
smudhiga <smudhiganti@phoenixcontact.com>
2022-02-27 21:43:21 -0500

    POST

--- app/calc/views.py
+++ app/calc/views.py
@@ -5,7 +5,7 @@ def home(request):
   return render(request, 'home.html', { 'name': 'Sai Krishna Reddy', 'title': 'Sai'})
 
 def add(request):
-  val1 = int(request.GET['num1'])
-  val2 = int(request.GET['num2'])
+  val1 = int(request.POST['num1'])
+  val2 = int(request.POST['num2'])
   res = val1 + val2
   return render(request, 'result.html', { 'res': res})
