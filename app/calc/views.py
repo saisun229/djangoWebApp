1690f277e078fa41390a0b3d2f4cbb57a8a67407
smudhiga <smudhiganti@phoenixcontact.com>
2022-02-27 21:38:03 -0500

    Addition calculation

--- app/calc/views.py
+++ app/calc/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
   return render(request, 'home.html', { 'name': 'Sai Krishna Reddy', 'title': 'Sai'})
+
+def add(request):
+  val1 = int(request.GET['num1'])
+  val2 = int(request.GET['num2'])
+  res = val1 + val2
+  return render(request, 'result.html', { 'res': res})
